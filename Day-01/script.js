926839b93793e697bb07129bf07e97faf3f4d7b0
MD. MIRAN HOSSAIN <sunmonwater826@gmail.com>
2026-05-08 08:40:28 +0600

    Enhance script.js with personal info and languages
    Added variables and an object to store personal information, along with an array of programming languages.

--- Day-01/script.js
+++ Day-01/script.js
@@ -1,8 +1,23 @@
-console.log("This is my first day of learning js")
+let name = "Miran";
+let age = 25;
+let isStudent = true;
+let favoriteProgrammingLanguage = "JavaScript";
 
-function init() {
-  document.getElementById("someId").innerText = "It's Sunny Day"
-}
+console.log ("Name: " + name);
+console.log ("Age: " + age);
+console.log ("Is Student: " + isStudent);
+console.log ("Favorite Programming Language: " + favoriteProgrammingLanguage);
+
+// create an Object and Array
+
+let person = {
+    name: "Miran Hossain",
+    age: 26,
+    isStudent: false,
+    favoriteProgrammingLanguage: "JavaScript and Python"
+};
+console.log(person);
 
 
-init();
+let programmingLanguages = ["JavaScript", "Python", "Java", "C++"];
+console.log(programmingLanguages);
